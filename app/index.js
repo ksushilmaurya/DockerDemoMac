b01fde664b578e3e07378f9e75a4bfc85563f732
sushil.kumar <ksushilmaurya@gmail.com>
2018-02-17 10:26:48 +0530

    initial commit mac trigger

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ var app 		= express();
 var mongo;
 //Define request response in root URL (/)
 app.get('/', function (req, res) {
-  res.send('Hello World! testing trigger again again again jhgjfdhgjfdhg')
+  res.send('Hello World! testing mac trigger')
 })
 
 app.get('/get_users', function (req, res) {
